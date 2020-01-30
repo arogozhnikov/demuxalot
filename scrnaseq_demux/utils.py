24fec7e9f369de6c804c09f33b45154c69e1ce97
Alex Rogozhnikov <iamfullofspam@gmail.com>
2020-01-29 16:51:37 -0800

    changed doublet posterior representation

--- scrnaseq_demux/utils.py
+++ scrnaseq_demux/utils.py
@@ -1,8 +1,12 @@
 import numpy as np
+import pandas as pd
 
 
 def hash_string(s):
-    """ used to compress UB (molecule barcodes) to group identical ones"""
+    """
+    Used to compress UB (molecule barcodes) to group identical ones.
+    Mapping is unique and fast for
+    """
     result = 0
     for c in s:
         result *= 5
@@ -22,3 +26,24 @@ class BarcodeHandler:
         assert len(set(barcodes)) == len(barcodes), "all passed barcodes should be unique"
         self.barcode2index = {bc: i for i, bc in enumerate(barcodes)}
         self.index2barcode = {i: bc for i, bc in enumerate(barcodes)}
+
+
+def read_vcf_to_header_and_pandas(vcf_filename):
+    """
+    :param vcf_filename: vcf filename
+    :return: list of commented lines (VCF header), and dataframe with all SNPs.
+        Positions in the output are 0-based
+    """
+    header_lines = []
+    with open(vcf_filename) as f:
+        for line in f:
+            if line.startswith("##"):
+                header_lines.append(line)
+            else:
+                break
+
+    df = pd.read_csv(vcf_filename, sep="\t", skiprows=len(header_lines))
+    assert list(df.columns[:8]) == ["#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"]
+    # switching to zero-based
+    df["POS"] -= 1
+    return header_lines, df.rename(columns={"#CHROM": "CHROM"})
