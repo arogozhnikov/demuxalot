6918563a83aefbfdec1fd5937fc7647aa4b1cff6
Alex Rogozhnikov <iamfullofspam@gmail.com>
2021-04-22 23:30:10 -0700

    - bump version in demuxalot - reimport detect_snp_positions to top-level

--- demuxalot/__init__.py
+++ demuxalot/__init__.py
@@ -1,6 +1,7 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Alex Rogozhnikov'
 
 from .utils import BarcodeHandler
 from .snp_counter import count_snps
 from .demux import Demultiplexer, ProbabilisticGenotypes
+from .snp_detection import detect_snps_positions
